d720bb9b6bf657094e641e377029b344e5501083
socorromarques <msocorrosmarques@gmail.com>
2025-06-11 13:24:49 +0100

    fix: alterando arquivos.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+
+export default function App() {
+
+  return (
+    <div className="flex h-screen items-center justify-center bg-red-200 text-pink-dark">
+      Olá, mundo!
+    </div>
+  )
+}
+
+
