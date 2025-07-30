310d67418f6749e860418d5571800bef188e6aa6
socorromarques <msocorrosmarques@gmail.com>
2025-07-30 08:35:54 +0100

    fix: Implementando lógica na aplicação.

--- src/core-components/task-item.tsx
+++ src/core-components/task-item.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ButtonIcon from "../components/button-icon";
 import Card from "../components/card";
 import InputCheckBox from "../components/input-checkbox";
@@ -8,48 +8,86 @@ import TrashIcon from "../assets/icons/trash.svg?react";
 import CheckIcon from "../assets/icons/check.svg?react";
 import XIcon from "../assets/icons/x.svg?react";
 import InputText from "../components/input-text";
+import { TaskState, type Task } from "../models/task";
+import { cx } from "class-variance-authority";
 
-export default function TaskItem() {
-    const [isEditing, setIsEditing] = React.useState(false);
+interface TaskItemProps {
+  task: Task;
+  onToggleTask: (taskId: string, isChecked: boolean) => void;
+}
 
-    function handleEditTask() {
-        setIsEditing(true);
-    }
+export default function TaskItem({ task }: TaskItemProps) {
+  const [isEditing, setIsEditing] = React.useState(
+    task?.state === TaskState.Creating
+  );
 
-    function handleExitEditTask() {
-        setIsEditing(false);
-    }
+  const [taskTitle, setTaskTitle] = React.useState("")
 
-    return(
-        
-        <Card size="md" className="flex items-center gap-4">
-            {!isEditing? (
-                <>
-                    <InputCheckBox />
-                    <Text className="flex-1">Curso React - Rocketseat</Text>
-                    <div className="flex gap-1">
-                        <ButtonIcon icon={TrashIcon} variant="tertiary" />
-                        <ButtonIcon 
-                          icon={PencilIcon} 
-                          variant="tertiary" 
-                          onClick={handleEditTask}
-                        />
-                    </div>
-                </>
-            ) : (
-                <>
-                    <InputText />
-                    <div className="flex gap-1">
-                        <ButtonIcon icon={XIcon} variant="secondary" onClick={handleExitEditTask} />
-                        <ButtonIcon 
-                          icon={CheckIcon} 
-                          variant="primary"
-                          onClick={handleExitEditTask}
-                        />
-                    </div>
-                </>
-            )}
-        </Card>
-        
-    )
+  function handleEditTask() {
+    setIsEditing(true);
+  }
+
+  function handleExitEditTask() {
+    setIsEditing(false);
+  }
+
+  function handleChangeTaskTitle(e: React.ChangeEvent<HTMLInputElement>) {
+    setTaskTitle(e.target.value);
+  }
+
+  function handleSaveTask(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    console.log({id: task.id, taskTitle});
+    // chamada para função de atualizar;
+    setIsEditing(false);
+  }
+
+
+  return (
+    <Card size="md">
+        <div className="flex items-center gap-4">
+          <InputCheckBox
+            value={task?.concluded?.toString()}
+            checked={task?.concluded}
+          />
+          <Text
+            className={cx("flex-1", {
+              "line-through": task?.concluded,
+            })}
+          >
+            {task?.title}
+          </Text>
+
+          <div className="flex gap-1">
+            <ButtonIcon icon={TrashIcon} variant="tertiary" />
+            <ButtonIcon
+            type="button"
+              icon={PencilIcon}
+              variant="tertiary"
+              onClick={handleEditTask}
+            />
+          </div>
+        </div>
+      
+        <form onSubmit={handleSaveTask} className="flex items-center gap-4">
+          <InputText 
+           className="flex-1" 
+           onChange={handleChangeTaskTitle} 
+           required 
+           autoFocus
+           />
+          <div className="flex gap-1">
+            <ButtonIcon
+              icon={XIcon}
+              variant="secondary"
+              onClick={handleExitEditTask}
+            />
+            <ButtonIcon
+              icon={CheckIcon}
+              variant="primary"
+            />
+          </div>
+        </form>
+    </Card>
+  );
 }
--- src/core-components/task-list.tsx
+++ src/core-components/task-list.tsx
@@ -1,31 +1,39 @@
 import Button from "../components/button";
 import PlusIcon from "../assets/icons/plus.svg?react";
-import TaskItem from "./task-item";
 import useTasks from "../hooks/use-tasks";
 import useTask from "../hooks/use.task";
+import TaskItem from "./task-item";
+import { TaskState } from "../models/task";
 
 export default function TaskLIst(){
     const {tasks} = useTasks();
     const {prepareTask} = useTask();
 
-    console.log(tasks);
 
     function handleNewTask() {
         prepareTask();
     }
 
     return(
-        <>
+      <>
         <section>
-            <Button icon={PlusIcon} className="w-full" onClick={handleNewTask}>Nova Tarefa</Button>
+            <Button icon={PlusIcon} 
+             className="w-full" 
+             onClick={handleNewTask}
+             disabled={tasks.some((task) => task.state === TaskState.Creating)}
+            >
+                Nova Tarefa
+            </Button>
         </section>
 
         <section className="space-y-4">
-            <TaskItem />
-            <TaskItem />
-            <TaskItem />
+            {tasks.map((task) => (
+                <TaskItem key={task.id} task={task} 
+                />
+            ))}
+            
         </section>
-        </>
+      </>
 
     )
 }
